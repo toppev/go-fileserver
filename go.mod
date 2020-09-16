cc69d33a50940fb76d10da3dc3ae771d2503b86d
toppev <44984598+toppev@users.noreply.github.com>
2020-09-16 23:01:19 +0300

    Add redis, some refactoring

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
-module toppe.dev/mediaserver
+module github.com/toppev/go-fileserver
 
 go 1.13
 
 require (
+	github.com/go-redis/redis/v8 v8.0.0
 	github.com/google/uuid v1.1.2
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.8.0
+	golang.org/x/exp v0.0.0-20200915172826-20d5ce0eab31 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -11,11 +14,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 var (
-	db *sql.DB
+	ctx   = context.Background()
+	db    *sql.DB
+	cache *redis.Client
 )
+
+// Cache expiration minutes
+const cacheExpiration = time.Minute * 60
 
 func main() {
 	log.Println("Starting server")
@@ -29,7 +38,18 @@ func main() {
 	if err != nil {
 		log.Fatal("Error connecting to the database", err)
 	}
-	_, _ = db.Exec("CREATE TABLE uploads (id SERIAL, fileId VARCHAR(36), cType VARCHAR(30), created date DEFAULT now(), bytes BYTEA)")
+	_, _ = db.Exec("CREATE TABLE uploads (id VARCHAR(36) PRIMARY KEY, cType VARCHAR(30), created date DEFAULT now(), bytes BYTEA)")
+
+	redisAddr := os.Getenv("REDIS")
+	if len(redisAddr) > 0 {
+		cache = redis.NewClient(&redis.Options{
+			Addr:     redisAddr,
+			Password: "",
+			DB:       0, // default DB
+		})
+		pong, err := cache.Ping(ctx).Result()
+		fmt.Println(pong, err)
+	}
 
 	http.HandleFunc("/upload", uploadHandler)
 	http.HandleFunc("/file/", getFileHandler)
@@ -43,6 +63,10 @@ func main() {
 }
 
 func uploadHandler(w http.ResponseWriter, req *http.Request) {
+	// Allow CORS: * or specific origin
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	err := req.ParseMultipartForm(1000 * 1024)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -50,7 +74,6 @@ func uploadHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	file, handler, err := req.FormFile("file")
-
 	defer file.Close()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -64,9 +87,8 @@ func uploadHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	body := UploadResponse{Data: []string{src}}
-
 	w.Header().Set("Content-Type", "application/json")
+	body := UploadResponse{Data: []string{src}}
 	json.NewEncoder(w).Encode(body)
 }
 
@@ -75,38 +97,88 @@ type UploadResponse struct {
 }
 
 func getFileHandler(w http.ResponseWriter, req *http.Request) {
-	fileID := req.RequestURI[len("/file/"):]
+	id := req.RequestURI[len("/file/"):]
 
-	bytes, cType, err := getFile(fileID)
+	bytes, cType, err := getFile(id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
-
 	w.Header().Set("Content-Type", cType)
 	w.Write(bytes)
 }
 
 func uploadFile(src io.Reader, cType string) (string, error) {
-	fileID := uuid.New().String()
-	bytes, err := ioutil.ReadAll(src)
+	id := uuid.New().String()
 
-	_, err = db.Exec("INSERT INTO uploads (fileId, cType, bytes) VALUES ($1, $2, $3)", fileID, cType, bytes)
+	bytes, err := ioutil.ReadAll(src)
 	if err != nil {
 		return "", err
 	}
-	
-	return os.Getenv("HOST") + "/file/" + fileID, nil
+
+	_, err = db.Exec("INSERT INTO uploads (id, cType, bytes) VALUES ($1, $2, $3)", id, cType, bytes)
+	if err != nil {
+		return "", err
+	}
+
+	return os.Getenv("HOST") + "/file/" + id, nil
 }
 
-func getFile(fileID string) ([]byte, string, error) {
-
-	row := db.QueryRow("SELECT bytes, cType FROM uploads WHERE fileID = $1", fileID)
+func getFile(id string) ([]byte, string, error) {
 
 	var bytes []byte
 	var cType string
+	var err error
 
-	err := row.Scan(&bytes, &cType)
+	if cache != nil {
+		if checkCache(id, &bytes, &cType) {
+			log.Println("Cache hit", id)
+			return bytes, cType, nil
+		} else {
+			log.Println("Cache miss", id)
+		}
+	}
+
+	row := db.QueryRow("SELECT bytes, cType FROM uploads WHERE id = $1", id)
+	err = row.Scan(&bytes, &cType)
+	if err != nil {
+		log.Println("Database error", err)
+	}
+
+	if cache != nil {
+		err = updateCache(id, cType, bytes)
+		if err != nil {
+			log.Println("Error with redis", err)
+		} else {
+			log.Println("New cache entry", id)
+		}
+	}
 
 	return bytes, cType, err
+}
+
+func updateCache(id string, cType string, bytes []byte) error {
+	err := cache.Set(ctx, "content_"+id, bytes, cacheExpiration).Err()
+	if err != nil {
+		return err
+	}
+	err = cache.Set(ctx, "type_"+id, cType, cacheExpiration).Err()
+	return err
+}
+
+func checkCache(id string, bytes *[]byte, cType *string) bool {
+
+	var err error
+
+	*cType, err = cache.Get(ctx, "type_"+id).Result()
+	if err == nil {
+		*bytes, err = cache.Get(ctx, "content_"+id).Bytes()
+		if err != nil {
+			log.Println("Cache error", "content_"+id, err)
+		}
+		return true
+	} else if err != redis.Nil {
+		log.Println("Cache error", "type_"+id, err)
+	}
+	return false
 }
